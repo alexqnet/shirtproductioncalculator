0123f5942e6a3046856e35d798c337c217ba7052
Alexander Quiñones <alexqeditor@gmail.com>
2021-07-27 19:10:07 -0400

    Add files via upload

---
+++ js/copy.js
@@ -0,0 +1,17 @@
+ 
+function CopyToClipboard(containerid) {
+  if (document.selection) {
+    var range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById(containerid));
+    range.select().createTextRange();
+    document.execCommand("copy");
+  } else if (window.getSelection) {
+    var range = document.createRange();
+    range.selectNode(document.getElementById(containerid));
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Quote Copied";
+  }
+  setTimeout(function(){ tooltip.innerHTML = "Copy"; }, 900);
+}
